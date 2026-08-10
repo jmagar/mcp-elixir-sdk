aca12120bea0d30a8d71c95cd60c522c68e40876
Jacob Magar <jmagar@gmail.com>
2026-08-10 03:50:41 -0400

    fix(deps): support Phoenix Req 0.5 lock

--- mix.exs
+++ mix.exs
@@ -156,7 +156,7 @@ defmodule MCPElixirSDK.MixProject do
       {:erlexec, "~> 2.3"},
 
       # Optional: Streamable HTTP transport
-      {:req, "~> 0.7", optional: true},
+      {:req, ">= 0.5.0 and < 0.8.0", optional: true},
       {:plug, "~> 1.16", optional: true},
       {:bandit, "~> 1.5", optional: true},
 
